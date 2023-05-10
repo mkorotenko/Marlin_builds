7e1e4e5d305c6ef9202932c1206f7d0406b0d0fb
Maxim Korotenko <m.v.korotenko@gmail.com>
2023-05-10 18:46:18 +0300

    Create STM32_board.js

---
+++ builds/STM32_board.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: "builds/marlin-build.js"
+    board_env: "STM32F103RE_creality",
+    active: true,
+    meta: {
+        stable_name: "ender_3_4.2.2-STM32-{{marlin_version}}-{{uid}}",
+        nightly_name: "ender_3_4.2.2-STM32-{{current_date}}-{{uid}}"
+    },
+    configuration: {
+    },
+    configuration_adv: {
+    }
+};
