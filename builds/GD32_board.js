76c95d975f9a51300faad26a85d300a4c595084c
Maxim Korotenko <m.v.korotenko@gmail.com>
2023-05-10 18:48:23 +0300

    Create GD32_board.js

---
+++ builds/GD32_board.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: "builds/marlin-build.js"
+    board_env: "GD32F303RET6_creality_maple",
+    active: true,
+    meta: {
+        stable_name: "ender_3_4.2.2-GD32-{{marlin_version}}-{{uid}}",
+        nightly_name: "ender_3_4.2.2-GD32-{{current_date}}-{{uid}}"
+    },
+    configuration: {
+    },
+    configuration_adv: {
+    }
+};
