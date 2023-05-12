6447f10c47b0966c6791fc7c838f6a48cb12222e
Maxim Korotenko <m.v.korotenko@gmail.com>
2023-05-12 23:02:09 +0300

    Update STM32_board.js

--- builds/STM32_board.js
+++ builds/STM32_board.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: "builds/marlin-build.js",
     board_env: "STM32F103RE_creality",
-    active: true,
+    active: false,
     meta: {
         stable_name: "ender_3_4.2.2-STM32-{{marlin_version}}-{{uid}}",
         nightly_name: "ender_3_4.2.2-STM32-{{current_date}}-{{uid}}"
