ef0101205ac07f9a51aaaf4674ddfa9295a04c0a
Maxim Korotenko <m.v.korotenko@gmail.com>
2023-05-22 11:00:10 +0300

    Update marlin-build.js

--- builds/marlin-build.js
+++ builds/marlin-build.js
@@ -1,6 +1,6 @@
 module.exports = {
     board_env: "STM32F103RE_creality",
-    active: true,
+    active: false,
     meta: {
         stable_name: "Marlin.2.0.x_4.2.2-STM-{{marlin_version}}-{{uid}}",
         nightly_name: "Marlin.2.1.2_4.2.2-STM-{{current_date}}-{{uid}}"
